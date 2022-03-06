5dffcaeebb57f94ee6f27fec5d90877d7272fe5b
MuhammadHaris2 <mhari8339@gmail.com>
2022-03-06 20:22:30 +0500

    string reverse

--- reverseString/index.js
+++ reverseString/index.js
@@ -4,39 +4,31 @@
 //   reverse('abcd') === 'dcba'
 //   reverse('Hello!') === '!olleH'
 
-function reverse(str) {}
+function reverse(str) {
+  // one Solution
+  // return str.split("").reverse().join("");
 
-// _________ _______  _______ _________   _______  _______  _______  _______  _______
-// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
-//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
-//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
-//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
-//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
-//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
-//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
-//                             ____       _
-//                             |  _ \     | |
-//                             | |_) | ___| | _____      __
-//                             |  _ < / _ \ |/ _ \ \ /\ / /
-//                             | |_) |  __/ | (_) \ V  V /
-//                             |____/ \___|_|\___/ \_/\_/
-//                         ______ ______ ______ ______ ______
-//                         |______|______|______|______|______|
+  //Alternate solution
+  //   let res = "";
+  //   for (let i = 0; i < str.length; i++) {
+  //     res = str[i] + res;
+  //   }
+  //   return res;
 
-//                          ______ ______ ______ ______ ______
-//                         |______|______|______|______|______|
+  // with reduce method
+  return str.split("").reduce((pre, next) => {
+    return next + pre;
+  }, "");
+}
 
-//                          ______ ______ ______ ______ ______
-//                         |______|______|______|______|______|
-
-mocha.setup('bdd');
+mocha.setup("bdd");
 const { assert } = chai;
 
-describe('String Reversal', () => {
-	it('reverse() correctly reverses string', () => {
-		assert.equal(reverse('ffaa'), 'aaff');
-		assert.equal(reverse('  aaff'), 'ffaa  ');
-	});
+describe("String Reversal", () => {
+  it("reverse() correctly reverses string", () => {
+    assert.equal(reverse("ffaa"), "aaff");
+    assert.equal(reverse("  aaff"), "ffaa  ");
+  });
 });
 
 mocha.run();
