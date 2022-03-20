4b8158f0418e6b839f08dda9635f928e02c72786
MuhammadHaris2 <mhari8339@gmail.com>
2022-03-20 10:13:14 +0500

    ceasarcipher

--- caesarCipher/index.js
+++ caesarCipher/index.js
@@ -8,30 +8,24 @@
 // caeserCipher("abcd", 100) === "wxyz";
 // caeserCipher("gurer ner 9 qbtf!", 13) === "there are 9 dogs!"
 
-function caesarCipher(str, shift) {}
-
-// _________ _______  _______ _________   _______  _______  _______  _______  _______
-// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
-//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
-//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
-//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
-//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
-//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
-//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
-//                             ____       _
-//                             |  _ \     | |
-//                             | |_) | ___| | _____      __
-//                             |  _ < / _ \ |/ _ \ \ /\ / /
-//                             | |_) |  __/ | (_) \ V  V /
-//                             |____/ \___|_|\___/ \_/\_/
-//                         ______ ______ ______ ______ ______
-//                         |______|______|______|______|______|
-
-//                          ______ ______ ______ ______ ______
-//                         |______|______|______|______|______|
-
-//                          ______ ______ ______ ______ ______
-//                         |______|______|______|______|______|
+function caesarCipher(str, shift) {
+  const alphabetArr = "abcdefghijklmnopqrstuvwxyz".split("");
+  let res = "";
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    const idx = alphabetArr.indexOf(char);
+    if (idx === -1) {
+      res += char;
+      continue; //its mean donot go further in this loop move on next iteration
+    }
+    const encodedIdx = (idx + shift) % 26; //because we have 26 character so if encode is 26 it retrun 0 26%26 =0 27%26=1 and so on
+    // if(encodedIdx > 25){
+    //   encodedIdx -=26
+    // }
+    res += alphabetArr[encodedIdx];
+  }
+  return res;
+}
 
 mocha.setup("bdd");
 const { assert } = chai;
